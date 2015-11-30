e8f78c8cd2314af1f5371ec238cc5c5c0f64143b
Erin Meyers <erinmeyers88@gmail.com>
2015-11-30 12:20:17 -0700

    Working on data display.

--- js/mainController.js
+++ js/mainController.js
@@ -18,14 +18,14 @@ angular.module("testApp")
             "kind": "qpxexpress#sliceInput",
             "origin": "PDX",
             "destination": "LAX",
-            "date": "2015-11-25",
-            "maxStops": 0,
-            "maxConnectionDuration": 0,
-            "preferredCabin": "COACH",
+            "date": "2015-12-15",
+            "maxStops": 10,
+            "maxConnectionDuration": 1440,
+            "preferredCabin": "",
             "permittedDepartureTime": {
-              "kind": "qpxexpress#timeOfDayRange",
-              "earliestTime": "01:00",
-              "latestTime": "23:00"
+              // "kind": "qpxexpress#timeOfDayRange",
+              // "earliestTime": "01:00",
+              // "latestTime": "23:00"
             },
             "permittedCarrier": [
               ""
@@ -38,7 +38,7 @@ angular.module("testApp")
         ],
         "maxPrice": "",
         "saleCountry": "",
-        "refundable": false,
+        "refundable": "",
         "solutions": 50
       }
     };
@@ -47,119 +47,132 @@ angular.module("testApp")
     //Set up the search function, which accepts an argument which is a request body.
     $scope.search = function (argument) {
       service.searchResults(argument).then(function (resultOfSearch) {
-        
-        //As
-        
+
+
+
         $scope.searchResults = resultOfSearch;
-        
-        console.log($scope.searchResults);
-        
-        //Push airlines and flight numbers into array of objects.
-		
-        $scope.airlineAndFlightNumbers = [];
-        $scope.legInfo = [];
-        
+
+
+
+
+
         $scope.searchResults.forEach(function (option1) {
+
+          option1.saleTotal = option1.saleTotal.replace("USD", "$");
+
           option1.slice.forEach(function (option2) {
+
+            option2.duration = option2.duration / 60;
+
             option2.segment.forEach(function (option3) {
               
-             //Push airline and flight number 
-              // $scope.airlineAndFlightNumbers.push(
-              //   {
-              //     airline: option3.flight.carrier,
-              //     flightNumber: option3.flight.number
-              //   }
-              //   );
+                var m = option3.duration % 60;
+                var h = (option3.duration - m) / 60;
+
+                option3.cleanDuration = h.toString() + ":" + (m < 10 ? "0" : "") + m.toString();
+              
             
-                
+
+              // option3.flight.carrier,
+              // option3.flight.number,
+                 
+
               option3.leg.forEach(function (option4) {
-                $scope.legInfo.push(
-                  {
-                    totalPrice: option1.saleTotal,
-                    totalDuration: option2.duration,
-                    airline: option3.flight.carrier,
-                    flightNumber: option3.flight.number,
-                    arrivalTime: option4.arrivalTime,
-                    departureTime: option4.departureTime,
-                    destination: option4.destination,
-                    origin: option4.origin,
-                    duration: option4.duration
-                  }
-                  );
-              });
 
+
+                option4.cleanDepartureTime = new Date(option4.departureTime);
+                    
+                // option4.origin,
+                    
+                option4.cleanArrivalTime = new Date(option4.arrivalTime);
+              
+                   
+                // option4.destination,
+                    
+                    
+                var m = option4.duration % 60;
+                var h = (option4.duration - m) / 60;
+
+                option4.cleanDuration = h.toString() + ":" + (m < 10 ? "0" : "") + m.toString();
+                    
+                // option4.duration
+                  
+
+              }
+                );
             });
+
           });
-          console.log($scope.searchResults)
-          console.log($scope.legInfo);
-        });
-       
 
-
-        //Push leg info into array of objects:
+          console.log($scope.searchResults);
+         
+         
+          //   //Push airlines and flight numbers into array of objects.
 		
-        
+          //   $scope.airlineAndFlightNumbers = [];
+          //   $scope.legInfo = [];
 
-    //     $scope.getLegInfo = function () {
-    //       $scope.searchResults.forEach(function (option1) {
-    //         option1.slice.forEach(function (option2) {
-    //           option2.segment.forEach(function (option3) {
-    //             option3.leg.forEach(function (option4) {
-    //               $scope.legInfo.push(
-    //                 {
-    //                   arrivalTime: option4.arrivalTime,
-    //                   departureTime: option4.departureTime,
-    //                   destination: option4.destination,
-    //                   origin: option4.origin,
-    //                   duration: option4.duration
-    //                 }
-    //                 );
-    //             });
-    //           });
-    //         });
-    //       });
-    //     } ();
-    //   });
-    // };
+       
+       
+          //   $scope.resultsList = [];
+
+          //   $scope.resultObject = {};
+
+          //   $scope.searchResults.forEach(function (option1) {
+
+          //     $scope.resultObject.totalPrice = option1.saleTotal;
+
+          //     option1.slice.forEach(function (option2) {
+
+          //       $scope.resultObject.totalDuration = option2.duration;
+
+          //       option2.segment.forEach(function (option3) {
+
+          //         $scope.resultObject.segments = [
+          //           {
+          //             airline: option3.flight.carrier,
+          //             flightNumber: option3.flight.number,
+          //           }
+          //         ];
+
+          //         option3.leg.forEach(function (option4) {
+
+          //           $scope.resultObject.segments[$scope.resultObject.segments.length-1].legs = [
+          //             {
+          //               arrivalTime: option4.arrivalTime,
+          //               departureTime: option4.departureTime,
+          //               destination: option4.destination,
+          //               origin: option4.origin,
+          //               duration: option4.duration
+          //             }
+          //           ];
+
+          //         }
+          //           );
+          //       });
+
+          //     });
+
+          //     $scope.resultsList.push($scope.resultObject);
 
 
-
-    
-
-
-    
-    
-    // $scope.airlineAndFlightNumbers = {};	
-
-    // $scope.getAirlineAndFlightNumbers = function () {
-    //   for (var i = 0; i < $scope.searchResults.length; i++) {
-    //     for (var j = 0; j < $scope.searchResults[i].length; j++) {
-    //       for (var k = 0; k < $scope.searchResults[i][j].length; k++) {
-    //            $scope.airlineAndFlightNumbers.push(
-    //           {
-    //             airline: $scope.searchResults[i][j][k].flight.carrier,
-    //             flightNumber: $scope.searchResults[i][j][k].flight.number
-    //           }
-    //         );
-    //       }
-    //     }
-    //   }
-    // console.log($scope.airlineAndFlightNumbers);
-    // }();
- 
-      
- 
-
+          //   });
+          //   console.log($scope.resultsList);
+          // });
 
 
 
+        });
+
+      });
 
 
+
+
+
+    };
+
+    //Call the search function and pass it the request body.
+    $scope.search($scope.requestBody);
 
   });
-  };
-  
-  //Call the search function and pass it the request body.
-    $scope.search($scope.requestBody);
- 
- });
--- js/service.js
+++ js/service.js
@@ -3,23 +3,25 @@ angular.module("testApp")
 		
 		this.searchResults = function (argument) {
 		
-		var deferred = $q.defer();	
+		// var deferred = $q.defer();	
 			
-		//regular key: AIzaSyAFSQP3ClWoPPShBYApLfxjazl-1WsKpu8	
+		//regular key: AIzaSyAFSQP3ClWoPPShBYApLfxjazl-1WsKpu8
+		//other key: AIzaSyCL0ZLFUF5_SsrocXX6ZKSaRlonngvd9cE	
 			
-			$http({
+			return $http({
 				method: 'POST',
-				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyCL0ZLFUF5_SsrocXX6ZKSaRlonngvd9cE',
+				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAFSQP3ClWoPPShBYApLfxjazl-1WsKpu8',
 				data: argument
 			}).then(function (response) {
 			
-			var searchResults = response.data.trips.tripOption;
+			return response.data.trips.tripOption;
+			
 						
-			deferred.resolve(searchResults);
+			// deferred.resolve(searchResults);
 			
 			});
 			
-			return deferred.promise;
+			// return deferred.promise;
 		};
 
 
